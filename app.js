340b69c573852e56dd0c5602332a9879e55acaab
Test User <sitedeneme20@gmail.com>
2020-03-27 22:49:33 +0200

    main-app

--- app.js
+++ app.js
@@ -45,7 +45,48 @@ http.createServer((request,response)=>{
 			for(var i = 0; i<posts.length; i++){
 				if(request.url == posts[i].blogURL){
 					//response.writeHead(200,{"Content-Type" : "text/html"})
-					response.write('<html><head><title>'+posts[i].title+'</title><meta charset = "utf-8"> <meta name = "viewport" content = "width = device-width , user-scalable = no"><meta name = "theme-color" content = "'+"settings.blog.theme_color"+'"><style>'+blogContentStyle+'</style></head><body><div class="container"><article><div id = "article-title">'+posts[i].subTitle+'</div><div id = "article-content">'+posts[i].content+'</div></article><div id= "comment"><form name = "comment" method="POST" action = "./send.php" onsubmit = "submit();"><h2><u>YORUM YAP</u> </h2><br/><b/><div><label>İsminiz: </label><br/><input type="text" name = "username" maxlength="100" spellcheck="false" required="true"><br/><br/></div><div><label>E - Mail : </label><br/><input type="email" name="email" maxlength="200" spellcheck="false" required="true"><br/><br/></div><div><label>Yaş : </label><br/><input type="text" maxlength="3" name="age" spellcheck="false" required="true"><br/><br/></div><div><label>YORUM : </label><br/><br/><textarea maxlength="65525" name="comment" spellcheck="false" required="true"></textarea></div><br/><br/><input type="submit" name="" id = "submit">&nbsp;&nbsp;<input type="reset" name="" id = "reset"></form></div></div></body></html>')
+					response.write(`
+					<html>
+					<head>
+						<title>'+posts[i].title+'</title>
+						<meta charset = "utf-8"> 
+						<meta name = "viewport" content = "width = device-width , user-scalable = no">
+						<meta name = "theme-color" content = "black">
+						<style>${blogContentStyle}</style>
+					</head>
+					<body>
+						<div class="container">
+							<article>
+								<div id = "article-title">${posts[i].subTitle}</div>
+								<div id = "article-content">${posts[i].content}</div>
+							</article>
+							<div id= "comment">
+								<form name = "comment" method="POST" action = "./send.php" onsubmit = "submit();">
+									<h2><u>YORUM YAP</u> </h2><br/><b/>
+									<div>
+										<label>İsminiz: </label><br/>
+										<input type="text" name = "username" maxlength="100" spellcheck="false" required="true"><br/><br/>
+									</div>
+									<div>
+										<label>E - Mail : </label><br/>
+										<input type="email" name="email" maxlength="200" spellcheck="false" required="true"><br/><br/>
+									</div>
+									<div>
+										<label>Yaş : </label><br/>
+										<input type="text" maxlength="3" name="age" spellcheck="false" required="true"><br/><br/>
+									</div>
+									<div>
+										<label>YORUM : </label><br/><br/>
+										<textarea maxlength="65525" name="comment" spellcheck="false" required="true"></textarea>
+									</div><br/><br/>
+									<input type="submit" name="" id = "submit">&nbsp;&nbsp;
+									<input type="reset" name="" id = "reset">
+								</form>
+							</div>
+						</div>
+					</body>
+					</html>`
+					)
 
 				}
 			}
@@ -54,9 +95,28 @@ http.createServer((request,response)=>{
 			
 
 		else if(request.url == "/"){
-			response.write('<html><style>'+blogStyle+'</style><head><title>BLOG</title><meta name = "viewport" content = "width=device-width user-scalable=no"><meta name = "theme-color" content = "black"><meta charset="utf-8"></head><header><div id = "name">'+"FURKAN <br/> BLOG"+ '</div><nav><a href = "https://www.instagram.com/furkan_gologlu/?hl=tr" target = "_blank">Instagram</a>|<a href = "#">Twitter</a></nav></header><body><div id = "posts">')	
+			response.write(`
+			<html>
+			<head>
+				<title>BLOG</title>
+				<style>${blogStyle}</style>
+				<meta name = "viewport" content = "width=device-width user-scalable=no">
+				<meta name = "theme-color" content = "black"><meta charset="utf-8">
+			</head>
+
+			<body>
+				${header}
+				<div id = "posts">`)	
+
 			for(var k = posts.length-1; k>=0; k--){
-					response.write('<div id = "post"><a href = "'+posts[k].blogURL+'" id = "title">'+posts[k].subTitle+'</a><div id = "description">'+posts[k].description+'</div><div id = "date">'+posts[k].date+'</div></div>');
+					response.write(`
+
+					<div id = "post">
+						<a href = "${posts[k].blogURL}" id = "title">${posts[k].subTitle}</a>
+						<div id = "description">${posts[k].description}</div>
+						<div id = "date">${posts[k].date}</div>
+					</div>`);
+
 				}
 			response.write('</div></body></html>');
 		}
